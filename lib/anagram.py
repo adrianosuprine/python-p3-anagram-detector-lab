3a0a811c2a00d2b02cc867547632b66199a60695
adrianosuprine <adrianosuprine@gmail.com>
2024-03-04 23:28:14 +0300

    anagram finder

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,30 @@
 # your code goes here!
+# pseudocode
+# start
+#   create empty list to store anagrams
+#   iterate over the list of possible anagrams
+#   compare each word on the list with the initialized one
+#   append any anagram found in the list
+#   return list of anagrams
+# end
+class Anagram:
+    # takes a word on initialization
+    def __init__(self,word):
+        self.word = word
+    
+    def match(self,possible_anagrams):
+        # creates empty list to store any anagrams
+        anagrams = []
+        for anagram in possible_anagrams:
+           
+            if sorted(self.word.lower()) == sorted(anagram.lower()):
+                anagrams.append(anagram)
+        return anagrams
+       
+            
+            
+            
+listen = Anagram("listen")
+listen.match(['enlists', 'google', 'inlets', 'banana'])
+# => ['inlets']            
+            
